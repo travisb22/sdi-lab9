2ebb72dfaea7868fb526bbdffb20f71b9b4560e6
travisb22 <travis.britt@ymail.com>
2014-03-22 15:44:42 -0400

    Lab 9 - 2

--- Britt_Travis_Lab9.js
+++ Britt_Travis_Lab9.js
@@ -5,36 +5,45 @@ var numAsString = "10";
 var date1 = "12/10/2014";
 var date2 = "12/12/2014";
 var d = "days";
+/*
+Problem 1 - Format a number
+Create a function that will format a number to a specified number of decimal places. 
+The function will need to accept two numbers: a number to be formatted and a number that represented decimal places. It should return the newly formatted number. Don't forget to output the number in a meaningful way.
 
+
+
+
+
+
+*/
 var numObject = function(num1,num2) {
 
 	var newNum = num1.toFixed(num2);
 
 	return newNum;
 
-		
+};
+console.log("The Number has succesfully been changed! " + numObject(num1,num2));
+
+var numString = function(numAsString){
+
+	var numAsStringValue = parseFloat(numAsString);
+	
+	return numAsStringValue;
+
+
+
+
+
 
 
 
 
 
 };
-console.log("The Number has succesfully been changed! " + numObject(num1,num2));
 
-var numString = function(numAsString){
+console.log("The string has been successfully changed to a number! " + numString(numAsString));
 
-
-
-
-
-
-
-
-
-
-
-
-}
 var dateType = function(date1,date2,d){
 	
 	
