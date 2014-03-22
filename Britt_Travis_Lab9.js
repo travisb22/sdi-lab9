8d1e3eafc0d3d7500835eb4d80720f1b9d7bb575
travisb22 <travis.britt@ymail.com>
2014-03-22 17:25:10 -0400

    Lab 9 - 4

--- Britt_Travis_Lab9.js
+++ Britt_Travis_Lab9.js
@@ -4,51 +4,52 @@ var num2 = 1;
 var numAsString = "10";
 var date1 = "12/10/2014";
 var date2 = "12/12/2014";
-var d = "days";
+var d = prompt("Enter days or hours");
+
 
 /*Problem 1 - Format a number
-Create a function that will format a number to a specified number of decimal places. 
+Create a function that will format a number to a specified number of decimal places.
 The function will need to accept two numbers: a number to be formatted and a number that represented decimal places.
 It should return the newly formatted number. Don't forget to output the number in a meaningful way.
 */
-var numObject = function(num1,num2) {
+var numObject = function(num1, num2) {
 
 	var newNum = num1.toFixed(num2);
 
 	return newNum;
 
 };
-console.log("The Number has succesfully been changed! " + numObject(num1,num2));
+console.log("The Number has succesfully been changed! " + numObject(num1, num2));
 
-var numString = function(numAsString){
+var numString = function(numAsString) {
 
 	var numAsStringValue = parseFloat(numAsString);
-	
+
 	return numAsStringValue;
 };
 
 console.log("The string has been successfully changed to a number! " + numString(numAsString));
 
-var dateType = function(date1,date2,d){
-	
+var dateType = function(date1, date2, d) {
+
 	var a = new Date(date1);
 	var b = new Date(date2);
-	var variationBetweenDates = a-b;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	var variationBetweenDates = a - b;
+
+	console.log(variationBetweenDates);
+
+	if (d === "hours") {
+
+		return ((variationBetweenDates / 1000) / 60) / 60;
+
+	} else if (d === "days") {
+
+
+		return (((variationBetweenDates/1000)/60)/60)/24;
+
+	}
+
 };
+
+console.log("The dates have been successfully changed " + dateType(date1, date2, d));
 
