438cc97c4c144529a7d5c4cad277a6ffa7b26d2d
travisb22 <travis.britt@ymail.com>
2014-03-22 15:06:08 -0400

    Lab 9 - 1

---
+++ Britt_Travis_Lab9.js
@@ -0,0 +1,54 @@
+//alert("JavaScript works!");
+var num1 = 5;
+var num2 = 1;
+var numAsString = "10";
+var date1 = "12/10/2014";
+var date2 = "12/12/2014";
+var d = "days";
+
+var numObject = function(num1,num2) {
+
+	var newNum = num1.toFixed(num2);
+
+	return newNum;
+
+		
+
+
+
+
+
+};
+console.log("The Number has succesfully been changed! " + numObject(num1,num2));
+
+var numString = function(numAsString){
+
+
+
+
+
+
+
+
+
+
+
+
+}
+var dateType = function(date1,date2,d){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+};
+
